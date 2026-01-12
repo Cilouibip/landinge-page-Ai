7d9348aeb28ccb7c75ec2faa9ae909451c43769c
Ciloui <mehdi@zenalacarte.com>
2026-01-12 10:05:58 +0100

    fix: update Systeme.io action URL and field names (first_name, surname, email)

--- app/actions/subscribe.ts
+++ app/actions/subscribe.ts
@@ -6,17 +6,16 @@ export async function subscribeToSystemeIo(formData: FormData) {
   const email = formData.get("email") as string
 
   try {
-    // Systeme.io utilise généralement une soumission de formulaire classique
-    // On va simuler la soumission avec les bons attributs name
-    const response = await fetch("https://www.mytribu.co/public/remote/page/36802753a0d81da794e0a728ce6c9becb6e7b31e.js", {
+    // Soumission au formulaire Systeme.io avec les noms de champs corrects
+    const response = await fetch("https://systeme.io/embedded/36802753/subscription", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: new URLSearchParams({
-        "form-input-fb648da8": lastName,
-        "field-4d9327a8": firstName,
-        "field-527f192d": email,
+        first_name: firstName,
+        surname: lastName,
+        email: email,
       }),
     })
 
